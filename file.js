2f0a21dfd7898b04cd9b73a667784cea07272a7c
saikrishna1909 <65270868+saikrishna1909@users.noreply.github.com>
2020-05-16 16:45:28 +0530

    Add files via upload

---
+++ file.js
@@ -0,0 +1,12 @@
+var name= ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim" ];
+
+for (var i = 0; i < name.length; i++) {
+
+var firstLetter = name[i].charAt(0).toLowerCase();
+
+if (       firstLetter === 'j'){ console.log("Good Bye" + name[i] )} else{ console.log("Hello" + name[i])}
+
+}
+
+
+
